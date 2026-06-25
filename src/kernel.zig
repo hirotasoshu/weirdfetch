470b576984fd071afdbb3f1b0cf65174e45eb176
Maksim Zayakin <rasetsuq@yandex.ru>
2026-06-25 23:20:20 +0500

    feat: support macOS kernel detection

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,18 +1,23 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const mem = std.mem;
 const fs = std.fs;
 
-pub fn getKernelVersion(alloc: *const mem.Allocator) ![]const u8 {
+pub fn getKernelVersion(alloc: mem.Allocator) ![]const u8 {
+    if (builtin.os.tag == .macos) {
+        const uts = std.posix.uname();
+        return try alloc.dupe(u8, mem.sliceTo(&uts.release, 0));
+    }
+
     const version = try fs.openFileAbsolute("/proc/version", .{ .mode = .read_only });
     defer version.close();
 
-    var buf_reader = std.io.bufferedReader(version.reader());
-    const reader = buf_reader.reader();
     var kernel_ver: []const u8 = "unknown";
 
-    var buf: [1024]u8 = undefined;
-    var info_line = try reader.readUntilDelimiterOrEof(&buf, '\n') orelse undefined;
-    var info_iterator = mem.tokenize(u8, info_line, " ");
+    const contents = try version.readToEndAlloc(alloc, 1024);
+    const line_end = mem.indexOfScalar(u8, contents, '\n') orelse contents.len;
+    const info_line = contents[0..line_end];
+    var info_iterator = mem.tokenizeAny(u8, info_line, " ");
     while (info_iterator.next()) |word| {
         if (mem.eql(u8, word, "version")) {
             kernel_ver = info_iterator.next().?;
